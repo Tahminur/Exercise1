905060cc076ce7dc76532ec556678582113ccc5c
Tahminur Rahman <trahman1@pride.hofstra.edu>
2020-06-18 11:34:55 -0400

    Initial Commit

---
+++ DevExercise1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevExercise1
+//
+//  Created by Tahminur Rahman on 6/18/20.
+//  Copyright © 2020 Tahminur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
